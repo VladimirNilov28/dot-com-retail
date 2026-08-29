3968d212f1525825158ecba6b68447fcaf528ca9
VladimirNilov28 <nilov.vova28@gmail.com>
2026-08-29 23:38:07 +0300

    red tests for datafetchers

--- backend/src/main/java/ee/bytecore/backend/graphql/datafetchers/UserDataFetcher.java
+++
@@ -1,8 +0,0 @@
-package ee.bytecore.backend.graphql.datafetchers;
-
-import com.netflix.graphql.dgs.DgsComponent;
-
-@DgsComponent
-public class UserDataFetcher {
-    public UserDataFetcher() {}
-}
